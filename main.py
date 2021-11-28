84dcd8b8f683c64dd5d47e8a004213b171cb34bc
Patrick <patrick.brook@outlook.com>
2021-11-28 13:53:56 +0000

    Fixed bug - program crashed if the last characters in the character list (alpha) were "pushed over" with the cipher. Now the character lists rotates, eg z + 1 = a, y + 3 = b etc

--- main.py
+++ main.py
@@ -50,12 +50,17 @@ def caesar():
         for y in alpha:
             # ...if a match, apply cipher and append to encrypted output.
             if x == alpha[alpha_cnt]:
+                # Rotates through character list if using a character at the end of the list.
+                if alpha_cnt + cipher[cipher_cnt] >= len(alpha):
+                    alpha_cnt = alpha_cnt - len(alpha)
+
                 enc = enc + alpha[alpha_cnt + cipher[cipher_cnt]]
                 # Rotate through cipher.
                 if cipher_cnt == len(cipher) - 1:
                     cipher_cnt = 0
                 else:
                     cipher_cnt = cipher_cnt + 1
+
             alpha_cnt = alpha_cnt + 1
         unenc_cnt = unenc_cnt + 1
 
